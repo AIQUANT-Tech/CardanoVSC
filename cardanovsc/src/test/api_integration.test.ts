e91de32ae4246b3a756958c318228f2d6739e914
Gaurav Raj <gaurav.raj@aiquantgroup.com>
2025-02-25 11:40:18 +0530

    fixing

--- cardanovsc/src/test/api_integration.test.ts
+++ cardanovsc/src/test/api_integration.test.ts
@@ -17,7 +17,7 @@ import("chai").then((chai) => {
     let showQuickPickStub: sinon.SinonStub;
     let showInputBoxStub: sinon.SinonStub;
     let globalStateUpdateStub: sinon.SinonStub;
-    const apiKey:string|undefined = process.env.CARDANO_API_KEY; // Fetch API key from .env
+    const apiKey: string | undefined = process.env.CARDANO_API_KEY; // Fetch API key from .env
 
     setup(() => {
       // Stub user input methods
@@ -35,24 +35,9 @@ import("chai").then((chai) => {
       sinon.restore(); // Restore stubbed methods after each test
     });
 
-    test("should return false if no network is selected", async () => {
-      // Mock user inputs
-      showQuickPickStub.resolves(undefined); // Simulate no selection
 
-      // Test the function
-      const result = await integrateCardanoAPI(
-        vscode,
-        {} as vscode.ExtensionContext
-      );
-
-      // Assertions
-      expect(result).to.be.false;
-    });
 
     test("should return false if no API key is entered", async () => {
-      // Mock user inputs
-      showQuickPickStub.resolves("preprod");
-      showInputBoxStub.resolves(undefined); // Simulate no input
 
       // Test the function
       const result = await integrateCardanoAPI(
@@ -64,17 +49,18 @@ import("chai").then((chai) => {
       expect(result).to.be.false;
     });
     test("should return true for valid API key", async () => {
-      const result = await validateApiKey(
-        apiKey,
-        "preprod"
-      );
+      if (apiKey) {
+        const result = await validateApiKey(
+          apiKey
+        );
 
-      expect(result).to.be.true;
+        expect(result).to.be.true;
+      }
     });
     test("should return false for invalid API key", async () => {
       const result = await validateApiKey(
-        "075b3c7ae74bf72046d609",
-        "preprod"
+        "075b3c7ae74bf72046d609"
+
       );
 
       expect(result).to.be.false;
@@ -82,8 +68,8 @@ import("chai").then((chai) => {
 
     test("should return false on API error", async () => {
       const result = await validateApiKey(
-        "075b3c7a458f72046d609",
-        "preprod"
+        "075b3c7a458f72046d609"
+
       );
 
       expect(result).to.be.false;
